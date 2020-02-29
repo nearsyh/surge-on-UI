a298d601a1c7aa3a370019dcd85cdeab48c3bbbc
Near <nearsy.h@gmail.com>
2020-02-29 19:17:18 +0800

    Create an empty page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ConfigManagement />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ConfigManagement from './components/ConfigManagement.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ConfigManagement
   }
 }
 </script>
---
+++ src/components/ConfigManagement.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="config-management">
+  </div>  
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
