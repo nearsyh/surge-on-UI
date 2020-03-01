4862fef224f9555a0584b871334921222432e852
Near <nearsy.h@gmail.com>
2020-03-01 12:12:31 +0800

    Add beforeMount to automatically show configuration

--- src/components/ConfigManagement.vue
+++ src/components/ConfigManagement.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="config-management">
     <ConfigCreation v-if="!configId" v-on:create-or-edit="createOrEdit" />
-    <ConfigEdit v-bind:config-data="configData" v-else  />
+    <ConfigEdit v-bind:config-data="configData" v-else />
   </div>
 </template>
 
 <script>
 import ConfigCreation from "./ConfigCreation.vue";
 import ConfigEdit from "./ConfigEdit.vue";
-import { createOrGetConfiguration } from "../lib/ConfigService";
+import {
+  createOrGetConfiguration,
+  getConfiguration
+} from "../lib/ConfigService";
 
 export default {
   components: {
     ConfigCreation,
     ConfigEdit
   },
-  props: ['configId'],
+  props: ["configId"],
   data: function() {
     return {
       configData: {
@@ -29,9 +32,10 @@ export default {
       }
     };
   },
-  beforeRouteUpdate (to, from, next) {
-    console.log(to);
-    next();
+  async beforeMount() {
+    if (this.configId) {
+      await this.refresh(this.configId);
+    }
   },
   methods: {
     createOrEdit: async function(id) {
@@ -39,6 +43,16 @@ export default {
       this.$router.push({
         path: `/config/${id}`
       });
+    },
+    async refresh(id) {
+      await getConfiguration(id).then(
+        res => {
+          this.configData = res;
+        },
+        () => {
+          alert("找不到对应的配置");
+        }
+      );
     }
   }
 };
